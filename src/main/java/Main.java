483205edb7f27d2aeaa0da65dba9cb66fac40873
unknown <awesome.hasanul@gmail.com>
2017-04-22 02:59:53 +0700

    output string

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,20 @@
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
 import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.TelegramBotsApi;
 import org.telegram.telegrambots.exceptions.TelegramApiException;
+import spark.ModelAndView;
+import spark.template.freemarker.FreeMarkerEngine;
+
+import java.io.IOException;
+import java.io.StringWriter;
 
 public class Main {
 
   public static void main(String[] args) {
+    testDrive();
+  }
+  private static void initTelegram(){
     ApiContextInitializer.init();
     TelegramBotsApi botsApi = new TelegramBotsApi();
 
@@ -14,5 +24,12 @@ public class Main {
       e.printStackTrace();
     }
   }
-
+  private static void testDrive(){
+     render("hello world");
+  }
+  private static String render(String output) {
+      StringWriter stringWriter = new StringWriter();
+      stringWriter.append(output);
+      return stringWriter.toString();
+  }
 }
