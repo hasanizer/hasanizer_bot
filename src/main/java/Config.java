cc3b0a741dd2efa21ff2428d2b930342afd2fe3e
unknown <awesome.hasanul@gmail.com>
2017-04-22 09:21:20 +0700

    ikutin aja dah

---
+++ src/main/java/Config.java
@@ -0,0 +1,6 @@
+/**
+ * Created by hasanizer on 4/22/2017.
+ */
+public class Config {
+    public static final String JDBC_DB_URL="jdbc:postgresql://ec2-54-221-244-196.compute-1.amazonaws.com:5432/d61bgq5cgjn2km?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory&user=gpwinumctfnukw&password=0ae4d6ab05e02ca8822fe2f62aa33de1831a143e5aa6d3d041d95720cd0ab3b3";
+}
