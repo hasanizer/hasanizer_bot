bcb0c0d283b12208a00f3d5737be4cc9f52620d3
unknown <awesome.hasanul@gmail.com>
2017-04-22 08:27:04 +0700

    harusnya bisa heroku

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -35,7 +35,7 @@ public class Main {
   }
   private static void testDrive(){
      render(System.getenv("JDBC_DB_URL"));
-     //renderDB();
+     renderDB();
   }
   private static void render(String output) {
       get("/", (request, response)-> {
